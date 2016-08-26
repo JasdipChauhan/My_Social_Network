6d10137faeeafa6a5b478ee5f868672bbfbf7057
JasdipC <jasdip.chauhan@gmail.com>
2016-08-25 23:50:06 -0400

    finished registration logic

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, g, render_template, flash, redirect, url_for
+from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask_login import LoginManager
 
-import models
 import forms
+import models
 
 DEBUG = True
 PORT = 8000
 HOST = '0.0.0.0'
 
 app = Flask(__name__)
-app.secret_key = 'afdsdafjdsfsdf.134asfdnanf!.fnjaiadf.asfadff'
+app.secret_key = 'auoesh.bouoastuh.43,uoausoehuosth3ououea.auoub!'
 
-login_manager = LoginManager();
+login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
@@ -22,38 +22,50 @@ def load_user(userid):
     except models.DoesNotExist:
         return None
 
+
 @app.before_request
 def before_request():
-    """Connect to the database before each request"""
+    """Connect to the database before each request."""
     g.db = models.DATABASE
     g.db.connect()
 
+
 @app.after_request
 def after_request(response):
-    """Close the database connection after each request"""
+    """Close the database connection after each request."""
     g.db.close()
     return response
+
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     form = forms.RegisterForm()
     if form.validate_on_submit():
-        flash('Register complete!', 'success')
-        models.User.create_user(username=form.username.data, email=form.email.data, password=form.password.data)
+        flash("Yay, you registered!", "success")
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
+
 
 @app.route('/')
 def index():
     return 'Hey'
 
 
+
 if __name__ == '__main__':
     models.initialize()
     try:
-        models.User.create_user(username="Jasdip", email="fake@fake.com", password='password', admin=True)
+        models.User.create_user(
+            username='kennethlove',
+            email='kenneth@teamtreehouse.com',
+            password='password',
+            admin=True
+        )
     except ValueError:
         pass
     app.run(debug=DEBUG, host=HOST, port=PORT)
-
-
--- models.py
+++ models.py
@@ -12,28 +12,24 @@ class User(UserMixin, Model):
     password = CharField(max_length=100)
     joined_at = DateTimeField(default=datetime.datetime.now)
     is_admin = BooleanField(default=False)
-
-    class Meta():
+    
+    class Meta:
         database = DATABASE
-        order_by = ('-joined_at')
-
-    #can create an instance of a user on this function call (meaning of classmethod)
+        order_by = ('-joined_at',)
+        
     @classmethod
     def create_user(cls, username, email, password, admin=False):
         try:
-            with DATABASE.transaction():
-                cls.create(
-                    username=username,
-                    email=email,
-                    password=generate_password_hash(password),
-                    is_admin=admin)
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
         except IntegrityError:
             raise ValueError("User already exists")
-
+            
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_table([User], safe=True)
+    DATABASE.create_tables([User], safe=True)
     DATABASE.close()
-
-
